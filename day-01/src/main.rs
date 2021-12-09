78694b9f1e032808288e1abb746eb8b904d7e3b3
Iván Zaera Avellón <ivan.zaera@liferay.com>
2021-12-09 12:35:56 +0100

    feat: part 2 of day 1

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,10 +1,13 @@
 use std::fs;
 
 fn main() {
-    let values = fs::read_to_string("/home/ivan/Liferay/Rust/advent-of-code-2021/day-01/input").unwrap();
-    let values : Vec<&str> = values.trim().split("\n").collect();
-    let values : Vec<u32> = values.iter().map(|val| -> u32 { val.parse().unwrap() }).collect();
+    let values = read_values();
+    let incs = find_increments_3(values);
 
+    dbg!(incs);
+}
+
+fn find_increments_1(values: Vec<u32>) -> u32 {
     let mut incs = 0;
     let mut prev = values[0];
 
@@ -16,5 +19,27 @@ fn main() {
         prev = values[i];
     }
 
-    dbg!(incs);
+    incs
+}
+
+fn find_increments_3(values: Vec<u32>) -> u32 {
+    let mut incs = 0;
+    let mut prev = values[0];
+
+    for i in 3..values.len() {
+        if values[i] > prev {
+            incs = incs + 1;
+        }
+
+        prev = values[i-2];
+    }
+
+    incs
+}
+
+fn read_values() -> Vec<u32> {
+    let values = fs::read_to_string("/home/ivan/Liferay/Rust/advent-of-code-2021/day-01/input").unwrap();
+    let values : Vec<&str> = values.trim().split("\n").collect();
+
+    values.iter().map(|val| -> u32 { val.parse().unwrap() }).collect()
 }
